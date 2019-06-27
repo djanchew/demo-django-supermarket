729a2a70accf0ea789ae52fe46ef49bfcf68b52c
陈建秋 <chenjianqiu@silanggame.com>
2019-06-27 17:47:09 +0800

    add(deploy): copy & modify deploy files

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""S_Web URL Configuration
+"""config URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
